eb56c3b8a1813e4193588fd5e3993b563abe167b
Jordon Terry <jt5493@hotmail.com>
2020-01-27 01:41:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == phrase.downcase
+  if phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
+  elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
